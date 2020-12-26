25e4999527f3f675dfbae90e816f80dcdf508da5
Arkajit Mondal <arkajit.18@gmail.com>
2020-12-27 02:25:11 +0530

    initial commit

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,9 @@
 module.exports = {
   googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123',
+    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
+  mongoURI:
+    "mongodb+srv://arkajit-nodejs:Arkajit_8474@mongodb-deploy.m3fkl.mongodb.net/adv-node-blog_dev?retryWrites=true&w=majority",
+  cookieKey: "123123123",
+  redisUrl: "redis://127.0.0.1:6379",
 };
