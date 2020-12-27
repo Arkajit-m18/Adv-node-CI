c2f88f42236ee583de543f5fb6c4769d59311860
Arkajit Mondal <arkajit.18@gmail.com>
2020-12-27 20:10:20 +0530

    added upload route

---
+++ routes/uploadRoutes.js
@@ -0,0 +1,31 @@
+const AWS = require("aws-sdk");
+const uuid = require("uuid/v1");
+
+const keys = require("../config/keys");
+const requireLogin = require("../middlewares/requireLogin");
+
+AWS.config = new AWS.Config({
+  accessKeyId: keys.accessKeyId,
+  secretAccessKey: keys.secretAccessKey,
+  region: "us-east-1",
+});
+
+const s3 = new AWS.S3();
+
+module.exports = (app) => {
+  app.get("/api/upload", requireLogin, (req, res) => {
+    const key = `${req.user.id}/${uuid()}.jpeg`;
+
+    s3.getSignedUrl(
+      "putObject",
+      {
+        Bucket: "adv-node-blog-bucket",
+        ContentType: "image/jpeg",
+        Key: key,
+      },
+      (err, url) => {
+        res.send({ key, url });
+      }
+    );
+  });
+};
